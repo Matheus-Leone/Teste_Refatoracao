05c108ccdcf42e8b094e9f60771bae3deea488c0
Matheus Leone <matheusleoneevangelista@gmail.com>
2025-11-26 20:30:30 -0300

    first commit

---
+++ calculadora/TesteCalculadora.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+public class TesteCalculadora {
+
+
+    public static void main(String[] args) {
+        calculadora calc = new calculadora();
+
+
+        //Testes rápidos
+        calc.calc(2, 3, "+"); // Esperado: 5
+        calc.calc(10, 4, "-"); // Esperado: 6
+        calc.calc(3, 5, "*"); // Esperado: 15
+        calc.calc(8, 2, "/"); // Esperado: 4
+        calc.calc(8, 0, "/"); // Esperado: divisão por zero
+        calc.calc(5, 5, "x"); // Esperado: operação inválida
+        
+    }
+
+}
